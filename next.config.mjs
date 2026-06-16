42c9139a30cf72826232bc0a5b3e0fd9b8f4096e
vanesabola <hiakong88999@gmail.com>
2026-06-16 16:50:25 +0700

    fix 413 body size limit 10mb

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  },
+}
 export default nextConfig
